a7a540bee1bd5a31a7a3d14846652d5a8c1dec87
ibnshayed <emranibnshayed@gmail.com>
2021-03-28 22:10:08 +0600

    jwt token, jwt customization

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,6 +4,10 @@ from . import views
 app_name = 'base'
 
 urlpatterns = [
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/profile/', views.getUserProfile, name='user_profile'),
+
+
     path("products/", views.getProducts, name='products'),
     path("products/<str:pk>/", views.getProduct, name='product'),
 
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,11 +1,38 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .products import products
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 # Create your views here.
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # data['username'] = self.user.username
+        # data['email'] = self.user.email
+
+        serializer = UserSerializerWithToken(self.user).data
+
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+@api_view()
+def getUserProfile(request):
+    user = request.user
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getRoutes(request):
