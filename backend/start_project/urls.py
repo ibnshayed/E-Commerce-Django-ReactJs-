e3cc6546a8ec7744e9be5828dd3b87024214afa7
ibnshayed <emranibnshayed@gmail.com>
2021-03-28 20:01:54 +0600

    drf simple jwt added

--- backend/start_project/urls.py
+++ backend/start_project/urls.py
@@ -4,11 +4,16 @@ from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    # TokenRefreshView,
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('api/users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('base.urls', namespace='base')),
 ]
 
