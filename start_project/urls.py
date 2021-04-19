0f14c85fb8cd98fafaf6374285e2c32a1132e7e0
ibnshayed <emranibnshayed@gmail.com>
2021-04-19 16:34:16 +0600

    media files problems solve

--- start_project/urls.py
+++ start_project/urls.py
@@ -1,13 +1,11 @@
 
 
 from django.conf import settings
-# from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.views.generic import TemplateView
-
-# from django.views.static import serve
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -20,12 +18,12 @@ urlpatterns = [
 
     path('api/orders/', include('base.urls.order_urls')),
 
-    # url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL,
