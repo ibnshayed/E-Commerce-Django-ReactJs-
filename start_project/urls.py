6d920b0429e361693c6897acded4d5e6857c1898
ibnshayed <emranibnshayed@gmail.com>
2021-04-19 14:50:26 +0600

    media files problems solve

--- start_project/urls.py
+++ start_project/urls.py
@@ -1,12 +1,12 @@
 
 
 from django.conf import settings
-from django.conf.urls import url
+# from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-from django.views.static import serve
+# from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
