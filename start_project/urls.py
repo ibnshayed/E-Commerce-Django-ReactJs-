e00c90496be37d73b49832780e1fab1db903b83f
ibnshayed <emranibnshayed@gmail.com>
2021-04-19 14:28:41 +0600

    media files problems solve

--- start_project/urls.py
+++ start_project/urls.py
@@ -19,12 +19,12 @@ urlpatterns = [
 
     path('api/orders/', include('base.urls.order_urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL,
