10d98256a8001779be1b4cd1de7b167d96e3cf07
ibnshayed <emranibnshayed@gmail.com>
2021-04-19 17:01:46 +0600

    media files problems solve

--- start_project/urls.py
+++ start_project/urls.py
@@ -1,11 +1,14 @@
 
 
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic import TemplateView
 from django.views.static import serve
+
+# from django.conf.urls.static import static
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# from django.contrib.staticfiles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -19,8 +22,14 @@ urlpatterns = [
     path('api/orders/', include('base.urls.order_urls')),
 
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    
+    # re_path(r'^static/(?P<path>.*)$', views.serve),
+    # re_path(r'^media/(?P<path>.*)$', views.serve,),
+    
 ]
+
+# urlpatterns += staticfiles_urlpatterns()
 
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
